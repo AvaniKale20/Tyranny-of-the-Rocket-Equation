e0e4963badf1b60413a78d66b9f8df54cdfc3177
Avani Kale <avani.kale@thoughtworks.com>
2020-03-05 11:03:55 +0530

    [avani]Add. should return calculation of sub fuel.

--- RocketModulePart2.py
+++ RocketModulePart2.py
@@ -1,4 +1,5 @@
 import math
+from math import floor
 import os
 
 file = os.path.expanduser('massInputForPart2')
@@ -8,8 +9,28 @@ with open(file, 'r') as massFile:
     print("Reading values from txt file---")
     print(values)
 
-#  we have to splits mass values
+# we have to splits mass values
 splittedValues = values.splitlines()
 print("splitted value--")
 print(splittedValues)
 print()
+
+calculated_fuel = 0
+
+
+def calculateOfSubFuel(x):
+    global calculated_fuel
+    fuel_value = floor((int(x) / 3) - 2)
+
+    if fuel_value < 0:
+        return fuel_value
+    print("fuel", fuel_value)
+    calculated_fuel = calculated_fuel + fuel_value
+    return calculateOfSubFuel(fuel_value)
+
+
+print("calculation of sub fuel")
+for n in splittedValues:
+    calculateOfSubFuel(n)
+print()
+print("calculation of sub fuel --",calculated_fuel)
